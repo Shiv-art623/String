36016f4b4bd76c4491853c2b529beade6dedbbd3
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2025-12-26 22:46:10 +0530

    Reverse a string

---
+++ ReverseString.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s ;
+    getline(cin,s);
+    cout<<s<<endl;
+    int st = 0, end = s.size()-1;
+    while(st<end){
+        swap(s[st], s[end]);
+        st++;
+        end--;
+    }
+    cout<<s;
+    return 0;
+}
