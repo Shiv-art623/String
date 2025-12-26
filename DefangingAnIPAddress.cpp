b7ab12babb7d0c31a4b0e0f2109778060c2c13d0
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2025-12-26 23:59:58 +0530

    mew

---
+++ DefangingAnIPAddress.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string address = "1.1.1.1";
+    string ans;
+    int index= 0;
+    while(index< address.size()){
+        if(address[index]== '.'){
+            ans = ans+"[.]";
+        }
+        else{
+            ans = ans+address[index];
+        }
+        index++;
+    }
+    cout<<ans;
+}
