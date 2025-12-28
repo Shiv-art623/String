b89fb8864583e28d82c1e8135cebb93f06d8ba1d
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2025-12-28 23:49:21 +0530

    check of Pangram

---
+++ CheckForPAngram.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPangram(string &sentense){
+    vector<bool> alpha(26,0);
+    for(int i =0; i<sentense.size(); i++){
+        alpha[sentense[i]-'a'] = 1;
+    }
+    for(int i =0; i<26; i++){
+        if(alpha[i] == 0){
+            return 0;
+        }
+    }
+ return 1;
+}
+int main(){
+    string s = "thequickbrownfoxjumpsoverthelazydog";
+    cout<<isPangram(s);
+    return 0;
+}
